c2bc212e44d7ab02fc5c768ad120660282760442
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-09-29 17:57:30 +0000

    conversion of datatypes

--- .devcontainer/01basics.js
+++ .devcontainer/01basics.js
@@ -8,3 +8,26 @@
 
 console.log(typeof(undefined)) // ot --> undefined
 console.log(typeof(null)) // ot --> object
+
+let s = 99
+console.log(typeof(s))
+
+let n = String(s) // conversion number into string
+console.log(typeof(n))
+
+s = "99qw"
+let str = Number(s) // not producing error
+console.log(typeof str);
+console.log(str); // print NaN but still it is a number type
+
+console.log(Number(null)) // ot --> 0
+console.log(Number(undefined)); // ot --> undefined
+console.log(Number(false)); // ot --> true = 1, false = 0
+
+// let isLoggedIn = 1 ot --> true
+// let isLoggedIn = "Shashi" ot --> true
+let isLoggedIn = "" // ot --> false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn) // ot --> true
