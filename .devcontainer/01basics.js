f7716bb5e51ab59fe5cc27033202aa4d04e7c788
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-09-29 17:34:07 +0000

    basic datatype

---
+++ .devcontainer/01basics.js
@@ -0,0 +1,10 @@
+"use script"; // treat all Js code as new syntax module or version
+
+// datatypes
+// null --> standalone value
+// bigInt --> bigger than normal integer
+// undefined -->
+// symbol --> unique
+
+console.log(typeof(undefined)) // ot --> undefined
+console.log(typeof(null)) // ot --> object
