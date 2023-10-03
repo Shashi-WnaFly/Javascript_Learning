bd6c9ca48cfa613cf7840514fd093a12e748f8aa
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-10-03 14:07:59 +0000

    more knowing about objects

---
+++ .devcontainer/objects2.js
@@ -0,0 +1,47 @@
+
+const newObj = new Object() // empty object
+const obj = {} // empty object
+
+let whatsappUser = {
+    id: "sha3323",
+    email: "somebody@gmail.com",
+    fullname: {
+        user_full_name:{
+            firstName: "Astro",
+            lastName: "Kamil"
+        }
+    },
+    password: "huhu22guy"
+}
+
+// console.log(whatsappUser);
+// console.log(whatsappUser.id);
+// console.log(whatsappUser.fullname.user_full_name.lastName);
+
+// object concatination
+
+let obj1 = {rock: "mountain", tree: "forest"}
+let obj2 = {paper: "copy", led: "pen"}
+
+// let obj3 = {obj1, obj2}; whole object copy as an object of obj3
+let obj3 = {...obj1, ...obj2} // --> spread operator
+// console.log(obj3);
+// console.log(obj1 === obj3); --> false
+
+// Object.freeze(obj1); object assignment not working
+// let obj3 = Object.assign({}, obj1, obj2);
+console.log(obj3);
+// console.log(obj1 === obj3); --> true, if i do not put there blank curly braces.
+
+// object methods
+
+// console.log(Object.keys(obj3)); // they all return array object
+// console.log(Object.values(obj3));
+// console.log(Object.entries(obj3));
+
+let keyValue = Object.keys(obj3);
+for(let i = 0; i < keyValue.length; i++){
+    if(keyValue[i] === "led") console.log("Hey! Hi led")
+}
+
+console.log(obj3.hasOwnProperty("paper")); // its only take key
