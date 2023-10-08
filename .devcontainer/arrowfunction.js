01b10bd634b60ea2e83ff993ae81f888bedc0721
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-10-08 17:22:35 +0000

    Learn about arrow function and this keyword and IIFE

---
+++ .devcontainer/arrowfunction.js
@@ -0,0 +1,42 @@
+
+let id = {
+    username: "Jhunjhun wala",
+    age: 55,
+    // print: console.log(this), // this will print empty object
+    info: function (){
+        console.log(`${this.username}, name of the client`);// if we use username it's raise an error
+        // console.log(this); // this will print whole id object
+    },
+}
+
+// id.info();
+// id.username = "Joker"
+id.info();
+id.print;
+
+// console.log();
+
+let manager = () => {
+    let managername = "tomato radish"
+    console.log(`this is the manager details`);
+    // console.log(`this is the name : ${this.managername}`); // not working
+    // console.log(this); // empty
+}
+
+manager()
+
+let sum = (num1, num2) => (num1 + num2);
+console.log(sum(23, 23));
+
+let pass = () => ({username: "Ravi Kumar"})
+console.log(pass())
+
+// IIFE --> Immediately invoked function execution
+
+(function name() {
+    console.log(`this is pass value`);
+})()
+
+// ( () => {
+//     console.log("A brown fox jumps over the lazy dog")
+// })
